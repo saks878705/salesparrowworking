1ed30c20c19534b2ef34f7b4a9f4fe07d41823a4
saks8787 <saksham.dubey@zoxima.com>
2022-10-21 16:05:02 +0530

    test

--- controllers/webservice/beatController.js
+++ controllers/webservice/beatController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Beat = mongoose.model("Beat");
 const Employee = mongoose.model("Employee");
+const Location = mongoose.model("Location");
 const Route = mongoose.model("Route");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
@@ -17,6 +18,7 @@ function get_current_date() {
 }
 
 router.post("/addBeat", (req, res) => {
+  console.log(req.body)
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if(!token){
@@ -28,70 +30,91 @@ router.post("/addBeat", (req, res) => {
   var decodedToken = jwt.verify(token, "test");
   var company_id = decodedToken.user_id;
   var beatName = req.body.beatName ? req.body.beatName : "";
+  var state = req.body.state ? req.body.state : "";
+  var city = req.body.city ? req.body.city : "";
   var employee_id = req.body.employee_id ? req.body.employee_id : "";
   var day = req.body.day ? req.body.day : "";
   var route_id = req.body.route_id ? req.body.route_id : "";
-    if (beatName != "") {
-      if (employee_id != "") {
-        if (day != "") {
-          if (route_id != "") {
-                Beat.find({ beatName: beatName })
-                  .exec()
-                  .then((beat_info) => {
-                    if (beat_info.length < 1) {
-                      var new_beat = new Beat({
-                        beatName: beatName,
-                        employee_id: employee_id,
-                        day: day,
-                        company_id:company_id,
-                        route_id: route_id,
-                        Created_date: get_current_date(),
-                        Updated_date: get_current_date(),
-                        status: "Active",
-                      });
-                      new_beat.save().then((data) => {
-                        res.status(200).json({
-                          status: true,
-                          message: "New Beat is created successfully",
-                          results: data,
+  if(state!=""){
+    if(city!=""){
+      if (beatName != "") {
+        if (employee_id != "") {
+          if (day != "") {
+            if (route_id != "") {
+                  Beat.find({ beatName: beatName })
+                    .exec()
+                    .then((beat_info) => {
+                      if (beat_info.length < 1) {
+                        var new_beat = new Beat({
+                          beatName: beatName,
+                          employee_id: employee_id,
+                          day: day,
+                          state: state,
+                          city: city,
+                          company_id:company_id,
+                          route_id: route_id,
+                          Created_date: get_current_date(),
+                          Updated_date: get_current_date(),
+                          status: "Active",
                         });
-                      });
-                    } else {
-                      res.status(401).json({
-                        status: true,
-                        message: "Beat already exists",
-                        result: null,
-                      });
-                    }
-                  });
+                        new_beat.save().then((data) => {
+                          res.status(200).json({
+                            status: true,
+                            message: "New Beat is created successfully",
+                            results: data,
+                          });
+                        });
+                      } else {
+                        res.status(401).json({
+                          status: true,
+                          message: "Beat already exists",
+                          result: null,
+                        });
+                      }
+                    });
+            } else {
+              return res.json({
+                status: false,
+                message: "Route id is required",
+                results: null,
+              });
+            }
           } else {
             return res.json({
               status: false,
-              message: "Route id is required",
+              message: "Day is required",
               results: null,
             });
           }
         } else {
           return res.json({
             status: false,
-            message: "Day is required",
+            message: "Employee id is required",
             results: null,
           });
         }
       } else {
         return res.json({
           status: false,
-          message: "Employee id is required",
+          message: "Beat Name is required",
           results: null,
         });
       }
-    } else {
+    }else{
       return res.json({
         status: false,
-        message: "Beat Name is required",
+        message: "City is required",
         results: null,
       });
     }
+  }else{
+    return res.json({
+      status: false,
+      message: "State is required",
+      results: null,
+    });
+  }
+    
 });
 
 router.post('/editBeat',(req,res)=>{
@@ -163,6 +186,7 @@ router.post('/getAllBeat',async (req,res)=>{
       if(beat_data.length>0){
         for(let i = 0;i<beat_data.length;i++){
           Employee.findOne({_id:beat_data[i].employee_id}).exec().then(emp_data=>{
+            console.log(beat_data[i].employee_id)
             Route.findOne({_id:beat_data[i].route_id}).exec().then(async (route_data)=>{
                 await (async function (rowData) {
                   var u_data = {
@@ -200,16 +224,41 @@ router.post('/getAllBeat',async (req,res)=>{
     })
 });
 
-// router.get('/getBeat',(req,res)=>{
-//     var id = req.body.id?req.body.id:"";
-//     Beat.find({_id:id}).exec().then(beat_data=>{
-//         res.json({
-//             status:true,
-//             message:"Beat found successfully",
-//             result:beat_data
-//         })
-//     })
-// });
+router.post('/getBeat',(req,res)=>{
+    var id = req.body.id?req.body.id:"";
+    Beat.findOne({_id:id}).exec().then(beat_data=>{
+      if(beat_data){
+        Location.find({_id:beat_data.state}).exec().then(state_data=>{
+          Location.find({_id:beat_data.city}).exec().then(city_data=>{
+            Employee.find({_id:beat_data.employee_id}).exec().then(emp_data=>{
+              Route.find({_id:beat_data.route_id}).exec().then(route_data=>{
+                var u_data = {
+                  id:beat_data._id,
+                  state:{name:state_data.name,id:beat_data.state},
+                  city:{name:city_data.name,id:beat_data.city},
+                  employee_name:emp_data.employeeName,
+                  route_name:{start_point:route_data.start_point,end_point:route_data.end_point},
+                  day:beat_data.day,
+                  beatName:beat_data.beatName
+                }
+                res.json({
+                  status:true,
+                  message:"data fetched",
+                  result:[u_data]
+                })
+              })
+            })
+          })
+        })
+      }else{
+        res.json({
+          status:false,
+          message:"Beat not found",
+          result:[]
+      })
+      }
+    })
+});
 
 // router.delete("/deleteBeat", (req,res) => {
 //     var id = req.query.id ?? "";
