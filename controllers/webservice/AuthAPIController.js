16843a32cb37a9eee74dfcb70e05b9dbe58be4db
saks8787 <saksham.dubey@zoxima.com>
2022-11-10 16:47:39 +0530

    test

--- controllers/webservice/AuthAPIController.js
+++ controllers/webservice/AuthAPIController.js
@@ -346,7 +346,7 @@ router.get('/getadminprofile',(req,res)=>{
   const token = authHeader && authHeader.split(" ")[1];
   console.log(token)
   console.log(typeof token)
-  if(token!="null" && token){
+  if( token){
     var decodedToken = jwt.verify(token, "test");
     var user_id = decodedToken.user_id;
     Admin.findOne({_id:user_id}).exec().then(admin_data=>{
