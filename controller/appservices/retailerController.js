f611f5d9d054bf9da27ae47b813d0f07d90cd73c
saks8787 <saksham.dubey@zoxima.com>
2022-11-14 12:46:41 +0530

    get all retailers

--- controller/appservices/retailerController.js
+++ controller/appservices/retailerController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Retailer = mongoose.model("Retailer");
 const Employee = mongoose.model("Employee");
+const Beat = mongoose.model("Beat");
 const router = express.Router();
 
 function get_current_date() {
@@ -53,7 +54,25 @@ router.post('/addRetailer',(req,res)=>{
                     if(mobileNo!=""){
                         if(DOB!=""){
                             if(DOA!=""){
-                                
+                                let new_retailer = new Retailer({
+                                    beat_id:beat_id,
+                                    customer_type:customer_type,
+                                    company_id:emp_data.companyId,
+                                    employee_id:emp_data._id,
+                                    pincode:pincode,
+                                    address:address,
+                                    firmName:firmName,
+                                    GSTNo:GSTNo,
+                                    customerName:customerName,
+                                    area:area,
+                                    location:location,
+                                    mobileNo:mobileNo,
+                                    DOB:DOB,
+                                    DOA:DOA,
+                                    Created_date:get_current_date(),
+                                    Updated_date:get_current_date(),
+                                    status:"Active"
+                                })
                             }else{
                                  res.json({
                                      status:false,
@@ -102,6 +121,85 @@ router.post('/addRetailer',(req,res)=>{
             message:"Beat must be selected"
         })
    }
+})
+
+router.post('/getAllRetailers',async (req,res)=>{
+    let beat = req.body.beat?req.body.beat:"";
+    let arr = [];
+    let limit = 10;
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if(!token){
+        return res.json({
+            status:false,
+            message:"Token must be provided"
+        })
+    }
+    var decodedToken = jwt.verify(token, "test");
+    var employee_id = decodedToken.user_id;
+    Employee.findOne({_id:employee_id}).exec().then(emp_data=>{
+        if(emp_data.status=="InActive" || emp_data.status=="UnApproved"){
+            return res.json({
+                status:false,
+                message:"You are Inactive. Please contact company."
+            }) 
+        }
+    })
+    if(beat!=""){
+        arr.push({company_id},{beat_id:beat})
+    }else{
+        arr.push({company_id})
+    }
+    let count = await Retailer.find({$and:arr});
+    Retailer.find({$and:arr}).exec().then(retailer_data=>{
+        if(retailer_data.length<1){
+            return res.json({
+                status:true,
+                message:"No retailers found",
+                result:[]
+                }) 
+        }else{
+            let list = [];
+            let countInfo = 0;
+            for(let i = 0;i<retailer_data.length;i++){
+                Beat.findOne({_id:retailer_data[i].beat_id}).exec().then(beat_data=>{
+                    await (async function(rowData){
+                        let data ={
+                            beat_id:beat_data.beatName,
+                            customer_type:rowData.customer_type,
+                            employee_id:emp_data.employeeName,
+                            company_id:rowData.company_id,
+                            pincode:rowData.pincode,
+                            address:rowData.address,
+                            firmName:rowData.firmName,
+                            GSTNo:rowData.GSTNo,
+                            customerName:rowData.customerName,
+                            area:rowData.area,
+                            location:rowData.location,
+                            mobileNo:rowData.mobileNo,
+                            DOB:rowData.DOB,
+                            DOA:rowData.DOA,
+                            status:rowData.status,
+                        }
+                        list.push(data)
+                    })(retailer_data[i])
+                    countInfo++;
+                    if(countInfo==party_data.length){
+                        let c = Math.ceil(count.length/limit);
+                        if(c==0){
+                           c+=1;
+                        }
+                        res.json({
+                          status:true,
+                          message:"Retailers found successfully",
+                          result:list,
+                          pageLength:c
+                      })
+                    }                
+                })
+            }
+        }
+    })
 })
 
 module.exports= router;
